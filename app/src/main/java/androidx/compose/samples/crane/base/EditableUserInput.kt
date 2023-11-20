bc5cec4b546fc150d789f8977cfcd7e62bc09a8f
RitaPintoBissApp <rpinto@blissapplications.com>
2023-11-20 16:52:32 +0000

    fix the bug in the EditableUserInput.kt

--- app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
+++ app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
@@ -23,7 +23,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -31,48 +30,25 @@ import androidx.compose.runtime.setValue
 import androidx.compose.samples.crane.ui.captionTextStyle
 import androidx.compose.ui.graphics.SolidColor
 
+
 @Composable
-fun CraneEditableUserInput(
-    hint: String,
-    caption: String? = null,
-    @DrawableRes vectorImageId: Int? = null,
-    onInputChanged: (String) -> Unit
-) {
-    // TODO Codelab: Encapsulate this state in a state holder
-    var textState by remember { mutableStateOf(hint) }
-    val isHint = { textState == hint }
-
-    CraneBaseUserInput(
-        caption = caption,
-        tintIcon = { !isHint() },
-        showCaption = { !isHint() },
-        vectorImageId = vectorImageId
-    ) {
-        BasicTextField(
-            value = textState,
-            onValueChange = {
-                textState = it
-                if (!isHint()) onInputChanged(textState)
-            },
-            textStyle = if (isHint()) {
-                captionTextStyle.copy(color = LocalContentColor.current)
-            } else {
-                MaterialTheme.typography.body1.copy(color = LocalContentColor.current)
-            },
-            cursorBrush = SolidColor(LocalContentColor.current)
-        )
+fun rememberEditableUserInputState(hint: String): EditableUserInputState =
+    rememberSaveable(hint, saver = EditableUserInputState.Saver) {
+        EditableUserInputState(hint, hint)
     }
-}
-
-
 
 class EditableUserInputState(private val hint: String, initialText: String) {
 
     var text by mutableStateOf(initialText)
+    private set
     /* É importante usar o mutableStateOf para que o Compose acompanhe as
     * mudanças no valor e faça a recomposição quando elas ocorrerem*/
     val isHint: Boolean
         get() = text == hint
+
+    fun updateText(newText: String) {
+        text = newText
+    }
 
     companion object {
         val Saver: Saver<EditableUserInputState, *> = listSaver(
@@ -87,8 +63,29 @@ class EditableUserInputState(private val hint: String, initialText: String) {
     }
 }
 
+
 @Composable
-fun rememberEditableUserInputState(hint: String): EditableUserInputState =
-    rememberSaveable(hint, saver = EditableUserInputState.Saver) {
-        EditableUserInputState(hint, hint)
+fun CraneEditableUserInput(
+    state: EditableUserInputState = rememberEditableUserInputState(""),
+    caption: String? = null,
+    @DrawableRes vectorImageId: Int? = null
+) {
+    CraneBaseUserInput(
+        caption = caption,
+        tintIcon = { !state.isHint },
+        showCaption = { !state.isHint },
+        vectorImageId = vectorImageId
+    ) {
+        BasicTextField(
+            value = state.text,
+            onValueChange = { state.updateText(it) },
+            textStyle = if (state.isHint) {
+                captionTextStyle.copy(color = LocalContentColor.current)
+            } else {
+                MaterialTheme.typography.body1.copy(color = LocalContentColor.current)
+            },
+            cursorBrush = SolidColor(LocalContentColor.current)
+        )
     }
+}
+
